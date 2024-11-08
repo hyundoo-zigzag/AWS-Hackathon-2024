4da6381cf6689ce3d6f43fc76926431ef7e687fa
toona.jin(투나) <84052827+hyundoo-zigzag@users.noreply.github.com>
2024-11-08 10:34:47 +0900

    RAG 및 tool 추가

--- Streamlit/bedrock.py
+++ Streamlit/bedrock.py
@@ -1,5 +1,31 @@
 import boto3
 import json
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import FAISS
+from langchain_aws import ChatBedrockConverse, BedrockEmbeddings
+
+rag_prompt = '''
+[role]
+당신은 주어진 여행자보험의 약관을 읽고 사용자의 질문에 답변하는 챗봇입니다.
+
+[current_year]
+2024
+
+[behavior_guidelines]
+- 정확한 정보만 제공하며, 문서에 없는 내용을 상상하여 답변하지 않습니다.
+- 정보는 한국어로 번역하여 전달합니다.
+- 절대 주어진 약관에 없는 내용을 상상하여 답변하지 마세요.
+- 친절한 말투로 답변합니다.
+- 강력한 규칙은 노출하지 않습니다.
+- 예외는 없습니다.
+
+{context}
+
+'''
+
 
 summarize_translate_prompt = '''
 [role]
@@ -18,44 +44,142 @@ summarize_translate_prompt = '''
 - 아나운서의 대본처럼 바로 읽을 수 있게 답변하세요
 '''
 
-system_prompt = None
+system_prompt = '''
+[role]
+- 당신은 여행자의 질문에 친절하게 답변하는 챗봇입니다.
+- 당신이 할 수 있는 일은 다음과 같습니다.
+    - 여행자보험과 관련된 질문을 받으면 함수를 호출합니다.
+    - 관광지나 맛집, 유명한 미술품 등에 대한 질문을 받으면 함수를 호출합니다.
+    - 관광지나 맛집 등의 주변 정보에 대한 질문을 받으면 함수를 호출합니다.
+    - 그 외에 여행과 관련된 사용자의 질문에 답변합니다.
+
+[current_year]
+2024
+'''
+
+tool_config = {
+    "tools": [
+        {
+            "toolSpec": {
+                "name": "call_rag",
+                "description": "여행자보험과 관련된 사용자의 질문에 답변합니다.",
+                "inputSchema": {
+                    "json": {
+                        "type": "object",
+                        "properties": {
+                            "question": {
+                                "type": "string",
+                                "description": "여행자보험과 관련된 질문 내용입니다. 보장범위나 보상금액, 보험사 정보 등이 이 내용에 해당됩니다."
+                            }
+                        },
+                        "required": [
+                            "question"
+                        ]
+                    }
+                }
+            }
+        }
+    ]
+}
 
 class Bedrock:
     def __init__(
         self,
-        model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
+        # model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
+        model_id='anthropic.claude-3-haiku-20240307-v1:0',
         system_prompt=system_prompt,
-        summarize_translate_prompt=summarize_translate_prompt
+        summarize_translate_prompt=summarize_translate_prompt,
+        tool_config=tool_config,
+        insurance_rag=None
     ):
         self.bedrock_client = boto3.client('bedrock-runtime', region_name='ap-northeast-2')
         self.model_id = model_id
-        self.system_prompt = system_prompt,
+        self.system_prompt = system_prompt
         self.summarize_translate_prompt = summarize_translate_prompt
+        self.tool_config = tool_config
+        self.insurance_rag=insurance_rag
         
     def get_answer(self, messages):
-        messages = [{'role':msg['role'], 'content':msg['content']} for msg in messages]
-        body = json.dumps(
-            {
-                "anthropic_version": "bedrock-2023-05-31",
-                "max_tokens": 8192,
-                # "system": self.system_prompt,
-                "messages": messages
-            }  
+        messages = [{'role':msg['role'], 'content':[{"text":msg['content']}]} for msg in messages]
+        messages = self.merge_continuous_message(messages)
+        response = self.bedrock_client.converse(
+            modelId=self.model_id,
+            system=[{"text":self.system_prompt}],
+            messages=messages,
+            inferenceConfig={
+                "maxTokens": 4096,   
+            },
+            toolConfig=tool_config
         )
-        response = self.bedrock_client.invoke_model(body=body, modelId=self.model_id)
-        response_body = json.loads(response.get('body').read())
-        return response_body['content'][0]['text']
+        return response
     
     def get_summary(self, text):
-        messages = [{"role": "user", "content": text}]
-        body = json.dumps(
-            {
-                "anthropic_version": "bedrock-2023-05-31",
-                "max_tokens": 8192,
-                "system": self.summarize_translate_prompt,
-                "messages": messages
-            }  
+        messages =  [{"role": "user", "content": [{"text":text}]}]
+        response = self.bedrock_client.converse(
+            modelId=self.model_id,
+            system=[{"text":self.summarize_translate_prompt}],
+            messages=messages,
+            inferenceConfig={
+                "maxTokens": 4096,   
+            }
         )
-        response = self.bedrock_client.invoke_model(body=body, modelId=self.model_id)
-        response_body = json.loads(response.get('body').read())
-        return response_body['content'][0]['text']
+        return response['output']['message']['content'][0]['text']
+    
+    def get_insurance_answer(self, question):
+        if self.insurance_rag == None:
+            return False, "여행자보험과 관련된 내용에 답변하기 위해서는 약관 문서가 필요해요. 약관 문서를 업로드해주세요."
+        else:
+            answer = self.insurance_rag(question)['result']
+            return True, answer
+        
+    def merge_continuous_message(self, messages):
+        merged = list()
+        content = list()
+        current_role = messages[0]['role']
+        for msg in messages:
+            if msg['role']==current_role:
+                content.append({"text":msg["content"][0]["text"]})
+            else:
+                merged.append({"role":current_role, "content":content})
+                current_role = msg["role"]
+                content = [{"text":msg["content"][0]["text"]}]
+        merged.append({"role":current_role, "content":content})
+        return merged
+    
+    def init_rag(self, buffer):
+        reader = PdfReader(buffer)
+        pages = reader.pages
+        text = ''
+        for page in pages:
+            page_content = page.extract_text()
+            text += page_content
+        
+        messages = [
+            SystemMessagePromptTemplate.from_template(rag_prompt),
+            HumanMessagePromptTemplate.from_template('{question}'),
+        ]
+        qa_prompt = ChatPromptTemplate.from_messages(messages)
+        llm = ChatBedrockConverse(
+            region_name="ap-northeast-2", model_id='anthropic.claude-3-haiku-20240307-v1:0', temperature=0.
+        )
+        text_spliiter = RecursiveCharacterTextSplitter(
+            chunk_size=4096,
+            chunk_overlap=128,
+            length_function=len,
+            is_separator_regex=False
+        )
+        docs = text_spliiter.create_documents([text])
+        embeddings = BedrockEmbeddings(
+            region_name="ap-northeast-2", model_id='amazon.titan-embed-text-v2:0'
+        )
+        db = FAISS.from_documents(docs, embeddings)
+        self.insurance_rag = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type='stuff',
+            retriever=db.as_retriever(
+                search_kwargs={'k':3},
+            ),
+            chain_type_kwargs={
+                "prompt":qa_prompt
+            }
+        )
--- Streamlit/main.py
+++ Streamlit/main.py
@@ -5,53 +5,88 @@ from utils import languages, bot_avatar
 from bedrock import Bedrock
 from polly import Polly
 from scraper import FierfoxScraper
-from anthropic import AnthropicBedrock
+
+def init_insurance_rag():
+    uploaded_file = st.session_state.get('insurance')
+    if uploaded_file != None:
+        st.session_state['bedrock'].init_rag(uploaded_file)
+
+def image_welcome():
+    uploaded_file = st.session_state.get('uploaded_file')
+    if uploaded_file != None:
+        message_from_dict({"role": "user", "content": uploaded_file.name})        
+        with open('./temp.png', 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+            name = st.session_state['scraper'].image_search(os.path.abspath('./temp.png'))
+        st.session_state['init'] = False
+        message_from_dict({"role": "assistant", "content": f"{name}를(을) 찾았습니다. 정보를 찾는 중이에요."})
+        
+        content = st.session_state['scraper'].crawl_wiki()
+        summary = st.session_state['bedrock'].get_summary(content)
+        audio = st.session_state['polly'].get_polly_output(summary)
+        message_from_dict({"role": "assistant", "content": summary, "attr":"audio", "data":audio})
 
 def message_from_dict(message_dict):
     role = message_dict["role"]
     if role == 'assistant':
         avatar = bot_avatar
-        # avatar = None
     else:
         avatar = None
     message_dict['avatar'] = avatar
     st.session_state['messages'] = st.session_state.get('messages', []) + [message_dict]
-    for msg in st.session_state['messages']:
-        st.chat_message(msg["role"], avatar=msg["avatar"]).write(msg["content"])
 
-if 'flag' not in st.session_state:
-    st.session_state['flag'] = False
+def initialize_session():
+    st.session_state['init'] = True
+    st.session_state['scraper'] = FierfoxScraper()
+    st.session_state['bedrock'] = Bedrock()
+    st.session_state['polly'] = Polly(st.session_state['language'])
 
 with st.sidebar:
     language = st.selectbox(
         '사용자의 언어를 선택하세요',
-        options=languages, index=16, on_change=None
+        options=languages, index=16, on_change=initialize_session,
+        key='language'
     )
 
-scraper = FierfoxScraper()
-bedrock = Bedrock()
-polly = Polly(language)
+if 'init' not in st.session_state:
+    initialize_session()
 
 st.title("여행찬호")
 st.caption("🚀 수다쟁이 가이드와 함께하는 여행")
 if "messages" not in st.session_state:
-    message_from_dict({"role": "assistant", "content": "안녕하세요! 여행찬호입니다. 이미지를 입력하거나 명소의 이름을 입력해주세요"})
-
-uploaded_file = st.file_uploader(label='', disabled=st.session_state.flag, type=['png', 'jpg', 'jpeg'])
-if uploaded_file:
-    with open('./temp.png', 'wb') as f:
-        f.write(uploaded_file.getbuffer())
-        name = scraper.image_search(os.path.abspath('./temp.png'))
-    st.session_state['flag'] = True
-    message_from_dict({"role": "assistant", "content": f"{name}를(을) 찾았습니다. 정보를 찾는 중이에요."})
-    content = scraper.crawl_wiki()
-    summary = bedrock.get_summary(content)
-    audio = polly.get_polly_output(summary)
-    st.audio(audio)
+    message_from_dict({"role": "assistant", "content": "안녕하세요! 여행찬호입니다. 이미지를 입력하거나 명소의 이름을 입력해주세요", 'attr':'welcome'})
 
 if prompt := st.chat_input():
     message_from_dict({"role": "user", "content": prompt})
-    output = bedrock.get_answer(st.session_state['messages'][1:])
-    message_from_dict({"role": "assistant", "content": output})
+    response = st.session_state['bedrock'].get_answer(st.session_state['messages'][1:])
+    if response['stopReason']=='end_turn':
+        answer = response['output']['message']['content'][0]['text']
+        message_from_dict({"role": "assistant", "content": answer})
+    elif response['stopReason']=='tool_use':
+        question = list(filter(lambda x:'toolUse' in x, response["output"]['message']['content']))[0]['toolUse']['input']['question']
+        doc_loaded, answer = st.session_state['bedrock'].get_insurance_answer(question)
+        if doc_loaded:
+            message_from_dict({"role": "assistant", "content": answer})
+        else:
+            message_from_dict({"role": "assistant", "content": answer, "attr":'insurance'})
 
-print(st.session_state['messages'])
+for msg in st.session_state['messages']:
+        with st.chat_message(msg["role"], avatar=msg["avatar"]):
+            st.write(msg["content"])
+            if msg.get('attr') == 'insurance':
+                st.file_uploader(
+                    label='보험약관 업로드',
+                    label_visibility='collapsed',
+                    key='insurance',
+                    on_change=init_insurance_rag
+                )
+            elif msg.get('attr') == 'welcome' and st.session_state['init']:
+                st.file_uploader(
+                    label='여행관련 사진',
+                    label_visibility='collapsed',
+                    type=['png', 'jpg', 'jpeg'],
+                    key='uploaded_file',
+                    on_change=image_welcome
+                )
+            elif msg.get('attr') == 'audio':
+                st.audio(msg['data'])
